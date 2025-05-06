a6bf0c30471f8b1414ca67bc10c93dce587dfaf4
Lisa-csg <lisa.casagrande.3@studenti.unipd.it>
2025-05-06 10:34:56 +0200

    Test3
    close #6

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -2,3 +2,43 @@
 // [Giulia] [Barzon] [2101074]
 // [Lisa] [Casagrande] [2116440]
 ////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntegerToRomanTest {
+    @Test
+    public void testConvert() {
+        //test 3
+        assertEquals("I", IntegerToRoman.convert(1));
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("III", IntegerToRoman.convert(3));
+
+        //test 6
+        
+
+        //test 10
+        
+
+        //test 20
+        
+
+        //test 50
+        
+
+        //test 100
+        
+
+        //test 500
+        
+
+        //test 1000
+        
+    }
+    
+    //Test con input <1 o >1000
+    
+}
--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -2,3 +2,43 @@
 // [Giulia] [Barzon] [2101074]
 // [Lisa] [Casagrande] [2116440]
 ////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RomanPrinterTest {
+    @Test
+    public void testPrint() {
+        //test 3
+        assertEquals("  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|\n", RomanPrinter.print(1));
+        assertEquals("  _____   _____ \n |_   _| |_   _|\n   | |     | |  \n   | |     | |  \n  _| |_   _| |_ \n |_____| |_____|\n", RomanPrinter.print(2));
+        assertEquals("  _____   _____   _____ \n |_   _| |_   _| |_   _|\n   | |     | |     | |  \n   | |     | |     | |  \n  _| |_   _| |_   _| |_ \n |_____| |_____| |_____|\n", RomanPrinter.print(3));
+    
+        //test 6
+        
+    
+        //test 10
+        
+
+        //test 20
+        
+
+        //test 50
+        
+
+        //test 100
+        
+    
+        //test 500
+        
+
+        //test 1000
+        
+    }
+
+    //Test con input <1 o >1000
+    
+}
