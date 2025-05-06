e83ccb959f2e072ef4b26d2a78039c73f2f685b9
Lisa-csg <lisa.casagrande.3@studenti.unipd.it>
2025-05-06 10:56:39 +0200

    Implementato Test 500
    close #12

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -46,7 +46,10 @@ public class IntegerToRomanTest {
         assertEquals("C", IntegerToRoman.convert(100));
 
         //test 500
-        
+        assertEquals("CC", IntegerToRoman.convert(200));
+        assertEquals("CCC", IntegerToRoman.convert(300));
+        assertEquals("CD", IntegerToRoman.convert(400));
+        assertEquals("D", IntegerToRoman.convert(500));
 
         //test 1000
         
